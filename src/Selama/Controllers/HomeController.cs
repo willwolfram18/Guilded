e4206e9fdb4f8b42129b917701047ad9bfd9f345
TheWolfA2 <man_of_magik903@yahoo.com>
2016-11-16 21:57:12 -0800

    Included newtonsoft.json

--- src/Selama/Controllers/HomeController.cs
+++ src/Selama/Controllers/HomeController.cs
@@ -3,31 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace Selama.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public ViewResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
+        public ViewResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Error()
+        public ViewResult Error()
         {
             return View();
         }
