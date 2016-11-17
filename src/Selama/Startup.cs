b06f63285a7716244293ba75cb5c0f61e2176652
TheWolfA2 <man_of_magik903@yahoo.com>
2016-11-16 21:31:24 -0800

    Initial commit of .NET Core version of Selama website to allow for more cross platform development

--- src/Selama/Startup.cs
+++ src/Selama/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Selama.Data;
 using Selama.Models;
 using Selama.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Selama
 {
@@ -52,7 +53,14 @@ namespace Selama
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc();
+            services.AddMvc(opts =>
+            {
+                opts.Filters.Add(new RequireHttpsAttribute());
+            });
+            services.AddRouting(opts =>
+            {
+                opts.LowercaseUrls = true;
+            });
 
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
@@ -81,16 +89,30 @@ namespace Selama
             app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseStaticFiles();
-
             app.UseIdentity();
+            app.UseGoogleAuthentication(new GoogleOptions
+            {
+                ClientId = "xxx",
+                ClientSecret = "xxx",
+            });
+            app.UseFacebookAuthentication(new FacebookOptions
+            {
+                ClientId = "xxx",
+                ClientSecret = "xxx",
+            });
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    "areaDefault",
+                    "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+                routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
---
+++ test/Selama.Tests/SampleUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Selama.Tests
+{
+    public class SampleUnitTest
+    {
+        [Fact]
+        public void Passed()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Failed()
+        {
+            Assert.True(false);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Theory(int number)
+        {
+            Assert.True(IsOdd(number));
+        }
+
+        private bool IsOdd(int number)
+        {
+            return number % 2 == 1;
+        }
+    }
+}
