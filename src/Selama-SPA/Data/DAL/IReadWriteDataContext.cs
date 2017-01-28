ebd2b298c24da72ab1f32e6399071cf617e6f1bf
TheWolfA2 <man_of_magik903@yahoo.com>
2017-01-27 23:35:16 -0800

    DAL carried over

---
+++ src/Selama-SPA/Data/DAL/IReadWriteDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Selama_SPA.Data.DAL
+{
+    public interface IReadWriteDataContext : IReadOnlyDataContext
+    {
+        void SaveChanges();
+        Task SaveChangesAsync();
+
+        bool TrySaveChanges();
+        Task<bool> TrySaveChangesAsync();
+    }
+}
