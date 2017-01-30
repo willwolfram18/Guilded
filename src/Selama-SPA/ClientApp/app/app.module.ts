e34b1a5f21d30f0c5d97af5913353f4b4dc1cbfd
Billy Wolfington <wolfington98@gmail.com>
2017-01-30 05:45:41 -0800

    Router module started

--- src/Selama-SPA/ClientApp/app/app.module.ts
+++ src/Selama-SPA/ClientApp/app/app.module.ts
@@ -4,12 +4,15 @@ import { UniversalModule } from "angular2-universal";
 import { SlimLoadingBarModule } from "ng2-slim-loading-bar";
 
 import { ProgressBarService } from "./core/services/progress-bar";
+import { RoutingModule } from "./core/modules/routing";
 
 import { AppComponent } from "./app.component"
 import { NavMenuComponent } from "./core/components/navmenu";
 import { HomeComponent } from "./root/components/home";
 import { GuildActivityComponent } from "./root/components/guild-activity";
 import { CounterComponent } from "./root/components/counter";
+
+import { HomeComponent as ForumsHomeComponent } from "./forums/components/home";
 
 @NgModule({
     bootstrap: [
@@ -20,7 +23,8 @@ import { CounterComponent } from "./root/components/counter";
         NavMenuComponent,
         CounterComponent,
         GuildActivityComponent,
-        HomeComponent
+        HomeComponent,
+        ForumsHomeComponent,
     ],
     providers: [
         ProgressBarService,
@@ -28,12 +32,7 @@ import { CounterComponent } from "./root/components/counter";
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         SlimLoadingBarModule.forRoot(),
-        RouterModule.forRoot([
-            { path: "", redirectTo: "home", pathMatch: "full" },
-            { path: "home", component: HomeComponent },
-            { path: "counter", component: CounterComponent },
-            { path: "**", redirectTo: "home" },
-        ]),
+        RoutingModule,
     ]
 })
 export class AppModule {
---
+++ src/Selama-SPA/ClientApp/app/forums/components/home.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "forums",
+    template: `<h3>Hello world, I'm the forums!</h3>`,
+})
+export class HomeComponent
+{
+}
