5ab7df90eca863841fda4d6d2fd090b22d27d255
TheWolfA2 <man_of_magik903@yahoo.com>
2017-03-20 21:21:24 -0700

    Created security project

---
+++ src/Guilded.Data/ViewModels/Core/Permission.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guilded.Data.ViewModels.Core
+{
+    public class Permission
+    {
+        #region Properties
+        #region Public properties
+        public string PermissionType { get; set; }
+
+        public string PermissionValue { get; set; }
+        #endregion
+        #endregion
+
+        public Permission()
+        {
+        }
+
+        public Permission(IdentityRoleClaim<string> roleClaim)
+        {
+            PermissionType = roleClaim.ClaimType;
+            PermissionValue = roleClaim.ClaimValue;
+        }
+    }
+}
---
+++ src/Guilded.Security/Claims/RoleClaimTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guilded.Security.Claims
+{
+    public static class RoleClaimTypes
+    {
+        public const string RoleManagement = "Guilded:Role Management";
+
+        public const string ForumsPinning = "Guilded:Forums Pinning";
+        public const string ForumsLocking = "Guilded:Forums Locking";
+        public const string ForumsReaded = "Guilded:Forums Reader";
+        public const string ForumsWriter = "Guilded:Forums Writer";
+    }
+}
