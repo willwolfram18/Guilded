0f19a0f417b9782a09b68bb13010e2477856613c
Billy Wolfington <wolfington98@gmail.com>
2017-03-26 10:35:52 -0700

    Guilded.Identity started

---
+++ src/Guilded.Identity/ApplicationRole.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Guilded.Identity
+{
+    public class ApplicationRole : IdentityRole
+    {
+    }
+}
