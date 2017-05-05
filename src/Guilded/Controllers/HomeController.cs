96a930ce84d47c43c9ef400b358c7600478435a8
Billy Wolfington <wolfington98@gmail.com>
2017-05-05 12:28:36 -0700

    reintegrating MVC app style tools

---
+++ src/Guilded/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guilded.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
