4f65216c518b26f681c002fd3e42b02b2e27425e
Bwolfing <wolfington98@gmail.com>
2017-05-11 07:44:21 -0700

    Simple image carousel on the home page

---
+++ src/Guilded/wwwroot/js/home/slideshow.ts
@@ -0,0 +1,14 @@
+﻿$(document).ready(() => {
+    const mySwiper = new Swiper(".swiper-container",
+        {
+            pagination: ".swiper-pagination",
+            paginationClickable: true,
+            nextButton: ".swiper-button-next",
+            prevButton: ".swiper-button-prev",
+            effect: "fade",
+            autoplayDisableOnInteraction: false,
+            centeredSlides: true,
+            loop: true,
+            autoplay: 4000
+        });
+});
