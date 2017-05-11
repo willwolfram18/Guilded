30ea9a8a3e28f47b07209a9e4cc98c398379e673
Billy Wolfington <wolfington98@gmail.com>
2017-05-11 12:21:08 -0700

    Fixed bower to include  Swiper; Added gulp to handle compiling less

---
+++ src/Guilded/gulpfile.js
@@ -0,0 +1,25 @@
+﻿/// <binding BeforeBuild='styles' />
+var gulp = require('gulp');
+var cleanCss = require("gulp-clean-css");
+var less = require("gulp-less");
+var path = require("path");
+var rename = require("gulp-rename");
+
+gulp.task("styles", function () {
+    var styleDirectory = "./wwwroot/css";
+
+    return gulp.src("./wwwroot/css/**/*.less")
+        .pipe(less({
+            paths: [path.join(__dirname, "wwwroot", "css")]
+        }))
+        .pipe(gulp.dest("./wwwroot/css"))
+        .pipe(cleanCss({
+            level: {
+                1: {
+                    specialComments: false,
+                }
+            }
+        }))
+        .pipe(rename({ suffix: ".min"}))
+        .pipe(gulp.dest("./wwwroot/css"));
+});
