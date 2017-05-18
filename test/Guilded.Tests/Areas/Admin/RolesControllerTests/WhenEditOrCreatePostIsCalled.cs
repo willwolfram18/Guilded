37181415b064a917e1dd6ff1d9f375ddba337fea
Bwolfing <wolfington98@gmail.com>
2017-05-18 07:49:03 -0700

    Fix a unit test

--- test/Guilded.Tests/Areas/Admin/RolesControllerTests/WhenEditOrCreatePostIsCalled.cs
+++ test/Guilded.Tests/Areas/Admin/RolesControllerTests/WhenEditOrCreatePostIsCalled.cs
@@ -35,101 +35,10 @@ namespace Guilded.Tests.Areas.Admin.RolesControllerTests
             MockAdminDataContext.Setup(db => db.UpdateRoleAsync(It.IsAny<ApplicationRole>()))
                 .ReturnsAsync(new ApplicationRole());
 
-            await Controller.EditOrCreate(new EditOrCreateRoleViewModel
-            {
-                ConcurrencyStamp = dbRole.ConcurrencyStamp
-            });
+            await Controller.EditOrCreate(new EditOrCreateRoleViewModel());
 
             MockAdminDataContext.Verify(db => db.UpdateRoleAsync(It.IsAny<ApplicationRole>()));
             MockAdminDataContext.Verify(db => db.CreateRoleAsync(It.IsAny<ApplicationRole>()), Times.Never);
-        }
-
-        [Test]
-        public async Task IfConcurrencyStampIsInvalidThenReturnCurrentRole()
-        {
-            var dbModel = new ApplicationRole
-            {
-                Name = "Current Name",
-                ConcurrencyStamp = "Current Stamp",
-                Claims =
-                {
-                    new IdentityRoleClaim<string>
-                    {
-                        ClaimType = RoleClaimTypes.ForumsReader.ClaimType
-                    },
-                    new IdentityRoleClaim<string>
-                    {
-                        ClaimType = RoleClaimTypes.ForumsWriter.ClaimType
-                    }
-                }
-            };
-            var modelToPost = new EditOrCreateRoleViewModel
-            {
-                Name = "Updated Name",
-                ConcurrencyStamp = "Old Stamp",
-                Permissions = new List<string>
-                {
-                    RoleClaimTypes.ForumsLocking.ClaimType
-                }
-            };
-
-            MockAdminDataContext.Setup(db => db.GetRoleById(It.IsAny<string>()))
-                .Returns(dbModel);
-
-            var result = await Controller.EditOrCreate(modelToPost);
-
-            var viewModel = result.Model as EditOrCreateRoleViewModel;
-
-            Assert.That(viewModel, Is.Not.Null);
-            Assert.That(viewModel.Name, Is.EqualTo(dbModel.Name));
-            Assert.That(viewModel.ConcurrencyStamp, Is.EqualTo(dbModel.ConcurrencyStamp));
-            Assert.That(viewModel.Permissions.Count, Is.EqualTo(dbModel.Claims.Count));
-
-            for (var i = 0; i < dbModel.Claims.Count; i++)
-            {
-                Assert.That(viewModel.Permissions[i], Is.EqualTo(dbModel.Claims.ElementAt(i).ClaimType));
-            }
-        }
-
-        [Test]
-        public async Task IfConcurrencyStampIsInvalidThenCreateAndUpdateAreNotCalled()
-        {
-            var dbModel = new ApplicationRole
-            {
-                ConcurrencyStamp = "New stamp"
-            };
-            var modelToPost = new EditOrCreateRoleViewModel
-            {
-                ConcurrencyStamp = "Old stamp"
-            };
-
-            MockAdminDataContext.Setup(db => db.GetRoleById(It.IsAny<string>()))
-                .Returns(dbModel);
-
-            await Controller.EditOrCreate(modelToPost);
-
-            MockAdminDataContext.Verify(db => db.CreateRoleAsync(It.IsAny<ApplicationRole>()), Times.Never);
-            MockAdminDataContext.Verify(db => db.UpdateRoleAsync(It.IsAny<ApplicationRole>()), Times.Never);
-        }
-
-        [Test]
-        public async Task IfConcurrencyStampIsInvalidThenViewDataErrorMessagesIsNotNull()
-        {
-            var dbModel = new ApplicationRole
-            {
-                ConcurrencyStamp = "New stamp"
-            };
-            var modelToPost = new EditOrCreateRoleViewModel
-            {
-                ConcurrencyStamp = "Old stamp"
-            };
-
-            MockAdminDataContext.Setup(db => db.GetRoleById(It.IsAny<string>()))
-                .Returns(dbModel);
-
-            var result = await Controller.EditOrCreate(modelToPost);
-
-            Assert.That(result.ViewData[ViewDataKeys.ErrorMessages]?.ToString(), Is.Not.Empty);
         }
     }
 }
