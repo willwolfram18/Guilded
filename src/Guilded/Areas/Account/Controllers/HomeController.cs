75bf30b65d621ee62153fd74ed2b673ddd2eb852
Billy Wolfington <wolfington98@gmail.com>
2017-05-23 13:22:31 -0700

    New users are automatically placed in the "Guest" role

--- src/Guilded/Areas/Account/Controllers/HomeController.cs
+++ src/Guilded/Areas/Account/Controllers/HomeController.cs
@@ -101,6 +101,7 @@ namespace Guilded.Areas.Account.Controllers
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "Guest");
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
                     // Send an email with this link
                     //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
