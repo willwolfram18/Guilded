b8c3ca923d1319b8d2f6cca0ebfe6cae077bfb77
Billy Wolfington <wolfington98@gmail.com>
2017-12-01 14:05:29 -0800

    Reworking markdown editor to custom editor

--- src/Guilded/wwwroot/js/shared/markdown-editor.ts
+++ src/Guilded/wwwroot/js/shared/markdown-editor.ts
@@ -1,61 +1,81 @@
-﻿let simpleMde: SimpleMDE;
+﻿function onMarkdownToolbarButtonClick(e: JQueryEventObject) {
+    let $target = $(e.target);
+    if (!$target.is(".button")) {
+        $target = $target.closest(".button");
+    }
 
-function convertMarkdown(content: string, previewElement?: HTMLElement): string {
-    const $markdownContainer = $(".markdown-editor");
+    const $markdownEditor = $target.closest(".markdown-editor");
+    const markdownAction = $target.data("markdown-action");
+    const $markdown = $markdownEditor.find(".markdown");
+
+    switch (markdownAction) {
+        case "preview":
+            togglePreview($markdownEditor, $markdown);
+            break;
+        case "guide":
+            $("#markdown-help").modal("toggle");
+            break;
+    }
+}
+
+function togglePreview($markdownEditor: JQuery, $markdown: JQuery) {
+    let $markdownPreview = $(".markdown-editor .markdown-preview");
+
+    $markdown.toggle();
+    $markdownPreview.toggle()
+        .text("Loading...");
 
     $.ajax({
         method: "POST",
-        url: $markdownContainer.data("markdown-action"),
+        url: $markdownEditor.data("markdown-action"),
         data: {
-            content: content
+            content: $markdown.val()
         },
-        error: () => {
-            previewElement.innerHTML = "An error occurred. Please try again."
-        },
-        success: (response) => {
-            previewElement.innerHTML = response;
-        }
+        error: () => $markdownPreview.html("An error occurred. Please try again."),
+        success: (response) => $markdownPreview.html(response)
     });
-
-    return "Loading...";
 }
 
 $(document).ready(() => {
-    simpleMde = new SimpleMDE({
-        autoDownloadFontAwesome: false,
-        blockStyles: {
-            bold: "__",
-            italic: "_"
-        },
-        element: $(".markdown-editor #Content")[0],
-        indentWithTabs: false,
-        parsingConfig: {
-            strikethrough: false
-        },
-        placeholder: "Content...",
-        previewRender: convertMarkdown,
-        tabSize: 4,
-        toolbar: [
-            "bold",
-            "italic",
-            "heading",
-            "|",
-            "quote",
-            "unordered-list",
-            "ordered-list",
-            "horizontal-rule",
-            "|",
-            "link",
-            "image",
-            "|",
-            "preview",
-            "|",
-            {
-                name: "guide",
-                action: () => $("#markdown-help").modal("toggle"),
-                className: "fa fa-question-circle",
-                title: "View markdown guide"
-            }
-        ]
-    });
+    $(".markdown-editor")
+        .on("click", ".markdown-toolbar .button", onMarkdownToolbarButtonClick)
+
+    $(".markdown-editor .markdown-preview").hide();
+    //simpleMde = new SimpleMDE({
+    //    autoDownloadFontAwesome: false,
+    //    blockStyles: {
+    //        bold: "__",
+    //        italic: "_"
+    //    },
+    //    element: $(".markdown-editor #Content")[0],
+    //    indentWithTabs: false,
+    //    parsingConfig: {
+    //        strikethrough: false
+    //    },
+    //    placeholder: "Content...",
+    //    previewRender: convertMarkdown,
+    //    tabSize: 4,
+    //    toolbar: [
+    //        "bold",
+    //        "italic",
+    //        "heading",
+    //        "|",
+    //        "quote",
+    //        "unordered-list",
+    //        "ordered-list",
+    //        "horizontal-rule",
+    //        "|",
+    //        "link",
+    //        "image",
+    //        "|",
+    //        "preview",
+    //        "|",
+    //        {
+    //            name: "guide",
+    //            action: () => $("#markdown-help").modal("toggle"),
+    //            className: "fa fa-question-circle",
+    //            title: "View markdown guide"
+    //        }
+    //    ]
+    //});
 });
